279eb7a626ad1437883fe87a5f9c4bc10506d704
Volodymyr Gamula <v.gamula@gmail.com>
2014-11-18 00:04:53 +0200

    Changed db to mysql

--- asd/asd/settings.py
+++ asd/asd/settings.py
@@ -61,8 +61,14 @@ WSGI_APPLICATION = 'asd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'asd',
+        'USER': 'asd',
+        'PASSWORD': 'asd',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
